71a533165b93cf3829a3b89f78c7e6407e7cdd17
PhysX-Times <40222617+PhysX-Times@users.noreply.github.com>
2018-06-14 21:40:34 +0800

    Revert "Revert "Added Remaining Weresquid Basic Functionals""
    This reverts commit 562180777675be5f0827d0597c843d7619e94775.

---
+++ Weresquid.h
@@ -0,0 +1,118 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Monster/Monster.h"
+#include "Weresquid.generated.h"
+
+class UAttackData;
+class ATrailMesh;
+class AHomingProjectile;
+class ASummonCaster;
+
+UCLASS()
+class LIBERTYPRIME_API AWeresquid : public AMonster
+{
+	GENERATED_BODY()
+	
+public:
+
+	AWeresquid();
+
+	int projectile_count;
+	int current_yaw;
+
+	int arrow_count;
+
+	float summon_yaw;
+	int summon_count;
+
+	ALibertyPrimeCharacter* Magic_Target;
+
+	UPROPERTY(BlueprintReadWrite, Category = "CppVariables")
+		FVector TeleportLocation;
+
+	UPROPERTY(EditAnywhere, Category = "CppVariables")
+		float AttackDistance_Staff;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CppVariables")
+		float EQS_Radius;
+
+	UPROPERTY(EditAnywhere, Category = "SubClasses")
+		TSubclassOf<ATrailMesh> TrailMeshClass;
+	UPROPERTY(EditAnywhere, Category = "SubClasses")
+		TSubclassOf<AHomingProjectile> HomingProjectileClass;
+	UPROPERTY(EditAnywhere, Category = "SubClasses")
+		TSubclassOf<AHomingProjectile> ArrowProjectileClass;
+	UPROPERTY(EditAnywhere, Category = "SubClasses")
+		TSubclassOf<ASummonCaster> SummonCasterClass;
+
+	UPROPERTY(EditAnywhere, Category = "Particles")
+		UParticleSystem* Teleport_PS;
+	UPROPERTY(Editanywhere, Category = "Particles")
+		UParticleSystem* Projectile_PS;
+
+	UPROPERTY(Editanywhere, Category = "Particles")
+		UParticleSystem* Sigil_PS;
+	UPROPERTY(Editanywhere, Category = "Particles")
+		UParticleSystem* Sigil_End_PS;
+
+	UPROPERTY(EditAnywhere, Category = "SoundCues")
+		USoundCue* SoundCue_Projectile;
+	UPROPERTY(EditAnywhere, Category = "SoundCues")
+		USoundCue* SoundCue_Teleport;
+
+	UPROPERTY(EditAnywhere, Category = "Montages")
+		UAnimMontage* TeleportMontage;
+
+	UPROPERTY()
+		UAttackData* MagicAttackData;
+	UPROPERTY()
+		UAttackData* ArrowAttackData;
+	UPROPERTY()
+		UAttackData* SummonAttackData;
+
+	UPROPERTY()
+		UParticleSystemComponent* SigilPS_Comp;
+
+	FTimerHandle MagicTimer;
+	FTimerHandle ArrowTimer;
+	FTimerHandle SummonTimer;
+
+	 void Magic_Teleport();
+
+	 UFUNCTION(BlueprintNativeEvent, Category = "CppFunctions")
+		 void Run_EQS();
+
+	 UFUNCTION(BlueprintCallable, Category = "CppFunctions")
+		 void Teleport_Notify();
+	 UFUNCTION(BlueprintCallable, Category = "CppFunctions")
+		 void Teleport_Apply();
+
+	 void Magic_Staff();
+	 void Magic_Arrow();
+	 void Magic_Summon();
+
+	 UFUNCTION(BlueprintCallable, Category = "CppFunctions")
+		void Projectile_Notify();
+
+	 UFUNCTION(BlueprintCallable, Category = "CppFunctions")
+		 void Arrow_Notify();
+
+	 UFUNCTION(BlueprintCallable, Category = "CppFunctions")
+		 void Summon_Notify();
+
+	 void SummonFunction();
+
+	 void SpawnProjectile();
+	 void SpawnArrow();
+
+	 void Sigil_Destory();
+
+	 void ResetDamager_Add() override;
+
+   	 virtual void BeginPlay() override;
+	 virtual void Tick(float DeltaTime) override;
+	
+};
